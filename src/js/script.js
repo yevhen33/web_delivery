18a6c392375f763544732f8c027d1b5c9ca899ec
Yevhen <yevhen.code@gmail.com>
2020-03-11 22:09:53 +0200

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -13,5 +13,12 @@ $(document).ready(function(){
     toggleSlide('.button_mini');
     toggleSlide('.button_back');
     
+    $('.reviews-carusel').slick({
+        speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/reviews/slayder-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/reviews/slayder-right.png"></button>'   
+      });
 });
  
