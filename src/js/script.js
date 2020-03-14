cdcb54789c2779a40d0670c5b01603080ecc0f91
Yevhen <yevhen.code@gmail.com>
2020-03-14 14:32:55 +0200

    add validate form, websate, mascinput

--- src/js/script.js
+++ src/js/script.js
@@ -20,5 +20,60 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/reviews/slayder-left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/reviews/slayder-right.png"></button>'   
       });
+
+    function validateForm(form){
+        $(form).validate({
+            rules: {
+              name: {
+                required: true,
+                minlength: 2
+              },
+              phone: "required",
+              email: {
+                required: true,
+                email: true
+              },
+              text: {
+                required: true,
+                minlength: 10
+              }
+            },
+            
+            messages: {
+              name: {
+                required: "Введите свое имя",
+                minlength: jQuery.validator.format("Введите минимум {0} символа!")
+              },
+              phone: "Введите свой номет телефона",
+              email: {
+                required: "Ведите почтовый ящик",
+                email: "Неверно введен email"
+              },
+              text: {
+                required: "Введите сообщение",
+                minlength: jQuery.validator.format("Введите минимум {10} символа!")
+              },
+            }
+        });
+    };
+
+    validateForm('.feed-form');
+    validateForm('.feed-form_question');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1200) {
+          $('.pageup').fadeIn();
+      } else {
+          $('.pageup').fadeOut();
+      }
+    });
+    $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    }); 
+
 });
  
